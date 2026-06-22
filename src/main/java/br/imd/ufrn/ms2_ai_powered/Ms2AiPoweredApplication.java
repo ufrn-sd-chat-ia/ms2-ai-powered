521e295be652a2ada4c2d199dea49017170eac4d
raquellzz <raquelcostafreire24@gmail.com>
2026-06-22 20:42:01 -0300

    First commit

---
+++ src/main/java/br/imd/ufrn/ms2_ai_powered/Ms2AiPoweredApplication.java
@@ -0,0 +1,13 @@
+package br.imd.ufrn.ms2_ai_powered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms2AiPoweredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms2AiPoweredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/imd/ufrn/ms2_ai_powered/Ms2AiPoweredApplicationTests.java
@@ -0,0 +1,13 @@
+package br.imd.ufrn.ms2_ai_powered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms2AiPoweredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
